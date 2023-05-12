4b9c3dd66fdf6d842664177a7f0ce5386774de22
kultvio <deromirk@gmail.com>
2023-05-12 15:25:52 +1000

    Hello World Triangle

--- EngineCore/src/window.cpp
+++ EngineCore/src/window.cpp
@@ -6,8 +6,41 @@
 #include "keyEvent.h"
 #include "windowEvent.h"
 #include <iostream>
-namespace GameEngine 
+namespace GameEngine
 {
+
+	GLfloat points[] = {
+		 0.0f,  0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f
+	};
+
+	GLfloat colors[] = {
+		1.0f, 0.0f, 0.0f,
+		0.0f, 1.0f, 0.0f,
+		0.0f, 0.0f, 1.0f
+	};
+
+	const char* vertex_shader =
+		"#version 460\n"
+		"layout(location = 0) in vec3 vertex_position;"
+		"layout(location = 1) in vec3 vertex_color;"
+		"out vec3 color;"
+		"void main() {"
+		"   color = vertex_color;"
+		"   gl_Position = vec4(vertex_position, 2.0);"
+		"}";
+	const char* fragment_shader =
+		"#version 460\n"
+        "in vec3 color;"
+        "out vec4 frag_color;"
+        "void main() {"
+        "   frag_color = vec4(color, 1.0);"
+        "}";
+
+	GLuint shader_program;
+	GLuint vao;
+
 	Window::Window(const std::string& name, int width, int height)
 	{
 		init(name, width, height);
@@ -58,6 +91,45 @@ namespace GameEngine
 		glfwSetKeyCallback(window, keyCallback);
 		glfwSetWindowCloseCallback(window, windowCloseCallback);
 		glfwSetWindowSizeCallback(window, windowSizeCallback);
+		//компилим шейдеры
+		GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vs, 1, &vertex_shader, nullptr);
+		glCompileShader(vs);
+
+		GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fs, 1, &fragment_shader, nullptr);
+		glCompileShader(fs);
+
+		shader_program = glCreateProgram();
+		glAttachShader(shader_program, vs);
+		glAttachShader(shader_program, fs);
+		glLinkProgram(shader_program);
+
+		glDeleteShader(vs);
+		glDeleteShader(fs);
+
+		GLuint points_vbo = 0;
+		glGenBuffers(1, &points_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_DYNAMIC_DRAW);
+
+		GLuint colors_vbo = 0;
+		glGenBuffers(1, &colors_vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_DYNAMIC_DRAW);
+
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+		return 0;
 
 	}
 	void Window::onUpdate()
@@ -65,6 +137,12 @@ namespace GameEngine
 		glClearColor(1, 0, 0, 0);
 		glClearColor(m_background_color[0], m_background_color[1], m_background_color[2], m_background_color[3]);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shader_program);
+		glBindVertexArray(vao);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		
+
 		ImGuiIO& io = ImGui::GetIO();
 
 		io.DisplaySize.x = width;
@@ -74,7 +152,6 @@ namespace GameEngine
 		ImGui_ImplGlfw_NewFrame();
 		ImGui::NewFrame();
 
-		ImGui::ShowDemoWindow();
 		ImGui::Begin("Background Color Window");
 		ImGui::ColorEdit4("Background Color", m_background_color);
 		ImGui::End();
@@ -127,6 +204,7 @@ namespace GameEngine
 		WindowResizedEvent e(width, height);
 		handle.width = width;
 		handle.height = height;
+		glViewport(0, 0, width, height);
 		handle.fnCallback(e);
 	}
 
